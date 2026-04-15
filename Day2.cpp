32bed6de5becd5fd3b4bc1d56ead7542bd94614d
divyanshu <divyanshuchaudhary067@gmail.com>
2026-04-15 18:14:12 +0530

    Ignore executable files

---
+++ Day2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int closestTarget(vector<string> &words, string target, int startIndex)
+{
+    int n = words.size();
+
+    for (int step = 0; step < n; step++)
+    {
+        int forward = (startIndex + step) % n;
+        int backward = (startIndex - step + n) % n;
+
+        if (words[forward] == target || words[backward] == target)
+        {
+            return step;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    return 0;
+}
