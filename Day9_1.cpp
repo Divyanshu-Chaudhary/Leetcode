8539a303fe3c4fc5b67ab299b5d098a87f4f0d34
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-04-22 13:53:09 +0530

    Two Sum

---
+++ Day9_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int comp = target - nums[i];
+
+        if (mp.find(comp) != mp.end())
+        {
+            return {mp[comp], i};
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main()
+{
+    return 0;
+}
