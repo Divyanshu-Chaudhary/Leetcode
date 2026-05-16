46c8c4a8a14b1ea614d5baf411bd7e30fc117432
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-05-16 16:18:08 +0530

    Largest Substring Between Two Equal Characters

---
+++ Day23_4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+int maxLengthBetweenEqualCharacters(string s)
+{
+    int n = s.length();
+    int result = -1;
+    // for(int i=0; i<n-1; i++){
+    //     for(int j=i+1; j<n; j++){
+    //         if(s[i]==s[j]){
+    //             result = max(result, j-i-1);
+    //         }
+    //     }
+    // }
+    // return result;
+    // unordered_map<char, int>mp;
+    // for(int i=0; i<n; i++){
+    //     char ch = s[i];
+    //     if(mp.find(ch)==mp.end()){
+    //         mp[ch] = i;
+    //     }
+    //     else{
+    //         result = max(result, i-mp[ch]-1);
+    //     }
+    // }
+    // return result;
+    vector<int> count(26, -1);
+    for (int i = 0; i < n; i++)
+    {
+        char ch = s[i];
+        if (count[ch - 'a'] == -1)
+        {
+            count[ch - 'a'] = i;
+        }
+        else
+        {
+            result = max(result, i - count[ch - 'a'] - 1);
+        }
+    }
+    return result;
+}
