374f18a8183c4a726995247ded00650972d4a037
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-05-23 15:30:25 +0530

    Sum of All Subset XOR Totals

---
+++ Day30_5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int solve(vector<int> &nums, int i, int Xor)
+{
+    if (i == nums.size())
+    {
+        return Xor;
+    }
+
+    int include = solve(nums, i + 1, nums[i] ^ Xor);
+    int exclude = solve(nums, i + 1, Xor);
+    return include + exclude;
+}
+
+int subsetXORSum(vector<int> &nums)
+{
+    // return solve(nums, 0, 0);
+
+    int n = nums.size();
+    int result = 0;
+    for (int &num : nums)
+    {
+        result |= num;
+    }
+    return result << (n - 1);
+}
