a5a0889ffbcec35d21526cd37515c4589a92060e
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-05-30 11:37:46 +0530

    Number Complement

---
+++ Day37_2(Number Complement).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Number Complement
+
+int findComplement(int num)
+{
+    int numbits = (int)(log2(num)) + 1;
+    unsigned int mask = (1U << numbits) - 1;
+    return num ^ mask;
+}
