6ca7c7e49b9e0344dd6d94d4ec69e16ca1e6211b
Divyanshu <140835290+Divyanshu-Chaudhary@users.noreply.github.com>
2026-06-03 12:31:08 +0530

    Final Prices With a Special Discount in a Shop

---
+++ Day41_3(Final Prices With a Special Discount in a Shop).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+// Final Prices With a Special Discount in a Shop
+
+vector<int> finalPrices(vector<int> &prices)
+{
+    int n = prices.size();
+    vector<int> result(begin(prices), end(prices));
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && prices[i] <= prices[st.top()])
+        {
+            result[st.top()] -= prices[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return result;
+}
