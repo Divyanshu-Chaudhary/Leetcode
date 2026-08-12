3a41462ee0d6d8f159fda3f2f8d299c6cc81fbe0
divyanshu <divyanshuchaudhary067@gmail.com>
2026-08-12 16:10:23 +0530

    Length of Longest Subarray With at Most K Frequency

---
+++ Day110(Length of Longest Subarray With at Most K Frequency).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+// Length of Longest Subarray With at Most K Frequency
+
+int maxSubarrayLength(vector<int> &nums, int k)
+{
+    int n = nums.size();
+
+    unordered_map<int, int> mp;
+
+    int i = 0;
+    int j = 0;
+    int result = 0;
+
+    while (j < n)
+    {
+
+        mp[nums[j]]++;
+
+        while (i < j && mp[nums[j]] > k)
+        {
+            mp[nums[i]]--;
+            i++;
+        }
+
+        result = max(result, j - i + 1);
+        j++;
+    }
+
+    return result;
+}
